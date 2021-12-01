692507c1cbb4662b653c6b1ee89382b59bc57897
Tomasz Sas <sastomasz@gmail.com>
2021-12-01 14:33:39 +0100

    simplify adding markers

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ MY_LOCATION = (50.061695, 19.936030)
 ZOOM = 15
 MAP_SOURCE = "Stamen Terrain"
 
+locations_file = pandas.read_excel("Locations.xlsx")
+lon = list(locations_file["Longitude"])
+lat = list(locations_file["Latitude"])
+
 # create base maps with Stamen Terrain service
 my_map = folium.Map(MY_LOCATION, zoom_start=ZOOM, tiles=MAP_SOURCE)
 
@@ -15,13 +19,8 @@ marker = folium.Marker(MY_LOCATION, "We start here")
 layer_1.add_child(marker)
 
 # add multiple markers from file
-locations_file = pandas.read_excel("Locations.xlsx")
-
-for i in range(locations_file.index.size):
-    longitude = locations_file.iloc[i, 0]   # first column in file with longitude
-    latitude = locations_file.iloc[i, 1]    # second column in file with latitude
-    location = (longitude, latitude)
-    layer_1.add_child(folium.Marker(location))
+for ln, lt in zip(lon, lat):
+    layer_1.add_child(folium.Marker((ln, lt)))
 
 # add layers to map
 my_map.add_child(layer_1)
