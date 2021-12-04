d703e496df105182aebcf6837bdacbde877ca258
Tomasz Sas <sastomasz@gmail.com>
2021-12-04 19:30:41 +0100

    add link to find info about volcanoes with google

--- main.py
+++ main.py
@@ -10,14 +10,16 @@ volcanoes_file = pandas.read_csv("Volcanoes.txt")
 volcanoes_lat = list(volcanoes_file["LAT"])
 volcanoes_lon = list(volcanoes_file["LON"])
 volcanoes_name = list(volcanoes_file["NAME"])
+volcanoes_elev = list(volcanoes_file["ELEV"])
 
-html = """<h4>Volcano information: </h4>
-Name: %s"""
+html = """Volcano name:<br>
+<a href="http://www.google.com/search?q=%%22%s%%22" target="blank">%s</a><br>
+Height: %s m"""
 
 
-def add_markers(lat, lon, name):
-    for lt, ln, nm in zip(lat, lon, name):
-        iframe = folium.IFrame(html=html % nm, width=100, height=50)
+def add_markers(lat, lon, name, elev):
+    for lt, ln, nm, el in zip(lat, lon, name, elev):
+        iframe = folium.IFrame(html=html % (nm, nm, el), width=200, height=100)
         layer_1.add_child(folium.Marker((lt, ln), popup=folium.Popup(iframe)))
 
 # create base maps with Stamen Terrain service
@@ -28,7 +30,7 @@ layer_1 = folium.FeatureGroup("pointer")
 marker = folium.Marker(MY_LOCATION, "We start here")
 layer_1.add_child(marker)
 
-add_markers(volcanoes_lat, volcanoes_lon, volcanoes_name)
+add_markers(volcanoes_lat, volcanoes_lon, volcanoes_name, volcanoes_elev)
 
 # add layers to map
 my_map.add_child(layer_1)
