eeb2de26e49bae8a890e2d02f71b304cf84cb03a
Tomasz Sas <sastomasz@gmail.com>
2021-12-04 13:17:24 +0100

    add popup with volcanoes name, delete custom markers

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from os import add_dll_directory
 import folium
 import pandas
 
@@ -7,18 +6,15 @@ MY_LOCATION = (50.061695, 19.936030)
 ZOOM = 15
 MAP_SOURCE = "Stamen Terrain"
 
-locations_file = pandas.read_excel("Locations.xlsx")
-locations_lat = list(locations_file["Latitude"])
-locations_lon = list(locations_file["Longitude"])
-
 volcanoes_file = pandas.read_csv("Volcanoes.txt")
 volcanoes_lat = list(volcanoes_file["LAT"])
 volcanoes_lon = list(volcanoes_file["LON"])
+volcanoes_name = list(volcanoes_file["NAME"])
 
 
-def add_markers(lat, lon):
-    for lt, ln in zip(lat, lon):
-        layer_1.add_child(folium.Marker((lt, ln)))
+def add_markers(lat, lon, name):
+    for lt, ln, nm in zip(lat, lon, name):
+        layer_1.add_child(folium.Marker((lt, ln), popup=nm))
 
 # create base maps with Stamen Terrain service
 my_map = folium.Map(MY_LOCATION, zoom_start=ZOOM, tiles=MAP_SOURCE)
@@ -28,8 +24,7 @@ layer_1 = folium.FeatureGroup("pointer")
 marker = folium.Marker(MY_LOCATION, "We start here")
 layer_1.add_child(marker)
 
-add_markers(locations_lat, locations_lon)
-add_markers(volcanoes_lat, volcanoes_lon)
+add_markers(volcanoes_lat, volcanoes_lon, volcanoes_name)
 
 # add layers to map
 my_map.add_child(layer_1)
